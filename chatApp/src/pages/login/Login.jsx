31277c58c7ebdf88a58ea0d52ba0994ffe40db2c
qwertyNippon <orlandohenry100@gmail.com>
2024-10-12 18:47:06 -0400

    update

--- chatApp/src/pages/login/Login.jsx
+++ chatApp/src/pages/login/Login.jsx
@@ -6,6 +6,20 @@ const Login = () => {
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className='logo'/>
+      <form className="login-form">
+        <h2>Sign Up</h2>
+        <input type='text' placeholder='Username' className="form-input" required />
+        <input type='email' placeholder='Email Address' className="form-input" />
+        <input type='password' placeholder='Password' className="form-input" />
+        <button type='submit'>Sign Up</button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toggle">Already have an account? <span>Click here.</span> </p>
+        </div>
+      </form>
     </div>
   )
 }
