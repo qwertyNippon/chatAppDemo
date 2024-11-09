3816dffee734f915c818d2ca0257399f52e49ca0
qwertyNippon <orlandohenry100@gmail.com>
2024-11-09 18:14:17 -0500

    update

--- chatApp/src/components/ChatBox/ChatBox.jsx
+++ chatApp/src/components/ChatBox/ChatBox.jsx
@@ -7,8 +7,40 @@ const ChatBox = () => {
     <div className='chat-box'>
       <div className="chat-user">
         <img src={assets.profile_img} alt="" />
-        <p>Orlando Henry <img src={assets.green_dot} alt="" /></p>
+        <p>Orlando Henry <img className='dot' src={assets.green_dot} alt="" /></p>
         <img src={assets.help_icon} className='help' alt="" />
+      </div>
+
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem quam soluta natus illo quis, iste porro fuga? Labore autem blanditiis ullam repellendus quaerat non maiores eius numquam, aliquam ratione.
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>TIME</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat autem quam soluta natus illo quis, iste porro fuga? Labore autem blanditiis ullam repellendus quaerat non maiores eius numquam, aliquam ratione.
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>TIME</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="chat-input">
+        <input type="text" placeholder='Send a Message' />
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
       </div>
     </div>
   )
